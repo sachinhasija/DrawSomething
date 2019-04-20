04f59571d81c25678829ed491d0d718ceb910a74
Sachin Hasija <sachin.khms@gmail.com>
2019-04-20 19:09:12 +0530

    add options feature

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,40 @@
 const canvas = document.querySelector("#drw");
 const ctx = canvas.getContext("2d");
 const clearCanvas = document.querySelector("#clearCanvas");
-ctx.strokeStyle = "red";
-ctx.lineCap = "round";
-ctx.lineJoin = "round";
-ctx.lineWidth = 100;
-let startX = 0,
-  startY = 0,
-  isDrawing = false,
-  hue = 0,
-  lineWidth = true;
+const strokeStyle = document.querySelector("#style");
+const color = document.querySelector("#color");
+const linewidth = document.querySelector("#linewidth");
+function drawCanvas(ctx, canvas, reset) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.strokeStyle = "red";
+  let startX = 0,
+    startY = 0,
+    isDrawing = false,
+    lineWidth = true;
 
-function draw(e) {
-  if (!isDrawing) return;
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.beginPath();
-  ctx.moveTo(startX, startY);
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
-  [startX, startY] = [e.offsetX, e.offsetY];
-  hue++;
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    lineWidth = !lineWidth;
+  function draw(e) {
+    if (!isDrawing) return;
+    ctx.strokeStyle = `${color.value}`;
+    ctx.beginPath();
+    ctx.lineCap = `${strokeStyle.value}`;
+    ctx.lineWidth = `${linewidth.value}`;
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [startX, startY] = [e.offsetX, e.offsetY];
   }
-  if (lineWidth) {
-    ctx.lineWidth++;
-  } else {
-    ctx.lineWidth--;
-  }
+  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mousedown", e => {
+    isDrawing = true;
+    [startX, startY] = [e.offsetX, e.offsetY];
+  });
+  canvas.addEventListener("mouseup", () => (isDrawing = false));
+  canvas.addEventListener("mouseout", () => (isDrawing = false));
+  reset.addEventListener("click", () => {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  });
 }
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", e => {
-  isDrawing = true;
-  [startX, startY] = [e.offsetX, e.offsetY];
-});
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
-clearCanvas.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+window.onload = () => {
+  drawCanvas(ctx, canvas, clearCanvas);
+};
